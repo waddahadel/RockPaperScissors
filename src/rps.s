c220d18c2bb06c107fa4ca4bf8a069cf88750fce
Wadah Elsir <wael00001@stud.uni-saarland.de>
2024-05-05 03:35:46 +0200

    game, print tape

--- src/rps.s
+++ src/rps.s
@@ -1,6 +1,8 @@
 # vim:sw=2 syntax=asm
 .data
-
+	W : .asciiz "W"
+	T : .asciiz "T"
+	L : .asciiz "L"
 .text
   .globl play_game_once
 
@@ -19,5 +21,74 @@
 #
 # Returns: Nothing, only print either character 'W', 'L', or 'T' to stdout
 play_game_once:
-  # TODO
+  
+  # free up the stackpointer to keep the $ra pointing to the right spot at first
+  # and get the move of player 1
+  addi $sp , $sp , -4
+  sw $ra , 0($sp)
+  
+  jal gen_byte
+  move $t1 , $v0
+  
+  lw $ra , 0($sp)
+  addi $sp, $sp , 4
+  
+  # do the same again to get the move of player two
+  
+  addi $sp , $sp , -4
+  sw $ra , 0($sp)
+  
+  jal gen_byte
+  move $t2 , $v0
+  
+  lw $ra , 0($sp)
+  addi $sp, $sp , 4
+
+  
+  #case distinguishing
+  beqz $t1 , cases_of_rock
+  beq $t1 , 1, cases_of_paper
+  beq $t1 , 2 , cases_of_scissors
+  
+  
+  
+  #basic algorithm
+cases_of_rock:
+	# just a register to implement the logic, maybe I'm writing too much code here, who knows?
+	sub $t3 , $t2 , $t1
+	beq $t1 , $t2 , tie
+	beq $t3 , 2 , win
+	bne $t3 , 2, lose
+	
+	
+cases_of_paper:
+	beq $t1 , $t2 , tie
+	beqz $t2 , win
+	bgt $t2 , $t1 , lose
+
+cases_of_scissors:
+	beq $t1, $t2 , tie
+	#introduce a dummy variable again
+	sub $t4 , $t1 , $t2
+	beq $t4 , $t1 , lose
+	bne $t4 , $t1 , win
+
+#winner winner chicken dinner?
+win:
+    la $a0 , W 
+    j terminate
+lose:
+    la $a0, L
+    j terminate
+tie:
+    la $a0 , T
+    j terminate
+    
+    
+#print the statement and terminate
+
+terminate:
+  li $v0 , 4
+  syscall
+  
   jr $ra
