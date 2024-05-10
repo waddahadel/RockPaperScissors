fe5857f722552fd3a76b0bc04a729eaa4b329e3a
Wadah Elsir <wael00001@stud.uni-saarland.de>
2024-05-10 11:48:25 +0200

    implemented the simulate automata test, now we move on

--- src/automaton.s
+++ src/automaton.s
@@ -17,47 +17,97 @@
 #
 # Returns: Nothing, but updates the tape in memory location 4($a0)
 simulate_automaton:
-  # Load necessary values from configuration
-  lw $t1, 4($a0)      # Load tape
-  lb $t2, 8($a0)      # Load tape_len
-  lb $t3, 9($a0)      # Load rule
-  
-  # Create a mask to extract neighborhoods
-  li $t4, 7           # Mask for 3 bits (111)
-  
-  # Create variables for current and next generation tapes
-  move $t5, $t1       # Current generation tape
-  move $t6, $zero     # Next generation tape
-  
-  # Loop through each cell in the tape
-  li $t0, 0           # Initialize counter
-  loop_simulate:
-    bge $t0, $t2, terminate_loop   # If reached tape_len, terminate
+    # Load the arguments into registers
+    lw $t1, 4($a0)       # Load tape (1 word)
+    lb $t2, 8($a0)       # Load tape_len (1 byte)
+    lb $t3, 9($a0)       # Load rule (1 byte)
+
+    move $t6 , $zero # this is our new tape.
+    li $t4 , 0   #our counter
+    subi $t2 , $t2 , 1 # for convenience. 
     
-    # Extract the neighborhood
-    and $t7, $t1, $t4      # Extract 3 bits neighborhood
-    andi $t7, $t7, 7       # Ensure we only consider 3 bits
+    initial_loop:
     
-    # Apply rule to determine next state
-    andi $t8, $t3, 1       # Extract LSB of rule
-    srl $t3, $t3, 1        # Shift right to process next neighborhood
-    add $t0, $t0, 1        # Increment counter
+    	beq $t4 , 0 , handle_first_digit # we are handeling the first digit.
+    	beq $t4 , $t2 , handle_last_digit # we are handeling the first digit.
+    	# normal digits (middle digits handeling)
+    	# first we will hendle intermediate results with $t5 , 
+    	# the idea is quite simple, shift to the right with the amout of t4
+    	# mask out the first three digits, if the digit indexed with their value in the rule is 1,
+    	# then we mark one, 
+    	# else we mark with zero.
+    	srlv $t5 , $t1 , $t4  # shifting to the right by the certain amount, store the intermediate result here.
+    	andi $t5 , 7 # getting the three first digits
+    	#again we shift the rul by t5 bits to get the bit to compare with I will use t0 for this
+    	
+    	srlv $t0, $t3, $t5 # now we have it
+    	
+    	andi $t0 , 1 # we extract it (it's the lease significant bit)
+    	# now whatever it's we just add it to that position of the tape! just a left shift, an or,and it's done.
+    	sllv $t0 , $t0 , $t4 # adjust it
+    	or $t6 , $t6 , $t0 # now just add it to that position in the new cell.
+    	# increment our counter and go back again
+    	addi $t4 , $t4 , 1
+    	
+    	j initial_loop
+    	    	
+    	
+    handle_first_digit:
+        # here , it's a bit complicated, but we will get it done.
+        # extract the first two digits
+         andi $t5, $t1 , 3
+         sll $t5 , $t5 , 1 #shift to the left to make space
+         # to get the most significant bit, we shift right by the amount of t2
+         srlv $t7 , $t1 , $t2 # its in t7
+         # or them
+         or $t5 , $t5 , $t7 
+         # do the check, increment the counter and go back. 
+         #again we shift the rule by t5 bits to get the bit to compare with I will use t0 for this
+    	
+    	srlv $t0, $t3, $t5 # now we have it
+    	
+    	andi $t0 , 1 # we extract it (it's the least significant bit) , note that we do the or directly, it's the first  bit !   
+        
+        or $t6 , $t6 , $t0 # now just add it to that position in the new cell.
+    	# increment our counter and go back again
+    	addi $t4 , $t4 , 1
+    	
+    	j initial_loop
     
-    # Update next generation tape
-    sll $t6, $t6, 1        # Shift left to make space for next cell
-    or $t6, $t6, $t8       # Set the bit according to the rule
+    handle_last_digit:
+         #it's also a bit complicated, but maybe simpler than the first bit?
+         # first grab the lsb
+         andi $t5 , $t1 , 1
+         # move by two to the left to make space!
+         sll $t5 , $t5 , 2
+         # we use our t4 to get the amount of shifts we need , t2 - 1
+         add $t4 , $t4, 1
+         # shift to the right by that amount.
+         srlv $t7 , $t1 , $t4
+         # extract the two last digits
+         andi $t7 , 3
+         # or with t5
+         or $t5 , $t5 , $t7
+         
+         # reqular routine, except that we will terminte!
+         #again we shift the rul by t5 bits to get the bit to compare with I will use t0 for this
+    	
+    	srlv $t0, $t3, $t5 # now we have it
+    	
+    	andi $t0 , 1 # we extract it (it's the lease significant bit)
+    	# now whatever it's we just add it to that position of the tape! just a left shift, an or,and it's done.
+    	# here it's the msb, 
+    	sllv $t0 , $t0 , $t2 # adjust it
+    	or $t6 , $t6 , $t0 # now just add it to that position in the new cell.
+    	
+    	#store the result in the desired memory location.
+    	
+    	sw $t6 , 4($a0)
+         
     
-    # Move to next cell
-    srl $t1, $t1, 1        # Shift tape to the right
     
-    # Repeat loop
-    j loop_simulate
+    	jr $ra
     
-  # Terminate loop
-  terminate_loop:
-    # Store next generation tape back to memory
-    sw $t6, 4($a0)
-    jr $ra
 
 # Print the tape of the cellular automaton
 # Arguments:
